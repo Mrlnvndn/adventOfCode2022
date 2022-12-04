9c8dc8ec20717f2ac695723b1563f317e906ae07
Merlijn van Uden <merlijn-van-uden@hotmail.nl>
2022-12-04 18:46:31 +0100

    day1 + day4

---
+++ day1/AdventDay1/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        AdventDay1.FindElf();
+
+        AdventDay1.Find3Elves();
+    }
+}
---
+++ day4/AdventDay4/Program.cs
@@ -0,0 +1,8 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        AdventDay4.ContainingPairs();
+        AdventDay4.OverlappingPairs();
+    }
+}
